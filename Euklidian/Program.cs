e5314051529caac57bc7ce894e942d4260fee96a
Mirco Stöpke <m.stoepke@gmail.com>
2023-03-30 20:13:31 +0200

    Extract the Function to an extension method

--- Euklidian/Program.cs
+++ Euklidian/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace Euklidian;
 
+using PeriodsToFractions;
+
 internal class Program
 {
     static void Main(string[] args)
@@ -15,39 +17,13 @@ internal class Program
             Console.WriteLine("Periodenlänge:");
             Int32.TryParse(Console.ReadLine(), out periodLength);
 
-            calculateFraction(number, periodLength, out int numerator, out int denumerator);
+            Console.WriteLine(number.ToFraction(periodLength));
 
-            Console.WriteLine("Die Bruchdarstellung lautet:");
-            Console.WriteLine($"{numerator}/{denumerator}");
-
+            Console.WriteLine();
             Console.WriteLine("Noch einmal? (j/n)");
             again = Console.ReadLine().Equals("j") ? true : false;
 
             Console.WriteLine();
         }
     }
-
-    private static void calculateFraction(double number, int periodLength, out int numerator, out int denumerator)
-    {
-        int numberOfDecimalPlaces = number.ToString().Split(",")[1].Length;
-        int factorA = (int)Math.Pow(10, Convert.ToDouble(numberOfDecimalPlaces));
-        int factorB = (int)Math.Pow(10, Convert.ToDouble(numberOfDecimalPlaces - periodLength));
-
-        numerator = (int)(number * Math.Pow(10, numberOfDecimalPlaces) - cutDecimals(number, numberOfDecimalPlaces - periodLength) * Math.Pow(10,numberOfDecimalPlaces - periodLength));
-        denumerator = factorA - factorB;
-
-        int ggt = ggT(numerator, denumerator);
-
-        numerator /= ggt;
-        denumerator /= ggt;
-    }
-
-    private static double cutDecimals(double number, int v)
-    {
-        (string, string) parts = (Math.Truncate(number).ToString(), number.ToString().Split(",")[1].Substring(0, v));
-        string strDecimal = $"{parts.Item1},{parts.Item2}";
-        return Double.Parse(strDecimal);
-    }
-
-    static int ggT(int a, int b) => (b == 0) ? a : ggT(b, a % b);
 }
---
+++ PeriodsToFractions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace PeriodsToFractions;
+public static class Extensions
+{
+    public static string ToFraction(this double number, int periodLength)
+    {
+        int numberOfDecimalPlaces = number.ToString().Split(",")[1].Length;
+        int factorA = (int)Math.Pow(10, Convert.ToDouble(numberOfDecimalPlaces));
+        int factorB = (int)Math.Pow(10, Convert.ToDouble(numberOfDecimalPlaces - periodLength));
+
+        int numerator = (int)(number * Math.Pow(10, numberOfDecimalPlaces) - cutDecimals(number, numberOfDecimalPlaces - periodLength) * Math.Pow(10, numberOfDecimalPlaces - periodLength));
+        int denumerator = factorA - factorB;
+
+        int ggt = ggT(numerator, denumerator);
+
+        numerator /= ggt;
+        denumerator /= ggt;
+
+        return $"{numerator}/{denumerator}";
+    }
+
+    private static int ggT(int a, int b) => (b == 0) ? a : ggT(b, a % b);
+
+    private static double cutDecimals(double number, int v)
+    {
+        (string, string) parts = (Math.Truncate(number).ToString(), number.ToString().Split(",")[1].Substring(0, v));
+        string strDecimal = $"{parts.Item1},{parts.Item2}";
+        return Double.Parse(strDecimal);
+    }
+}
